dba20c00998527c2a0f2b6ca4173e676b7bdcc88
Ammundhe <97632239+Ammundhe@users.noreply.github.com>
2022-04-16 17:19:39 -0700

    Header About us

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 import './MenuItem.css';
 
 const MenuItem = () => (
-  <div>
-    MenuItem
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className='p__cormorant' style={{color:"#DCCA87"}}>title</p>
+      </div>
+      <div className='app__menuitem-dash'/>
+      <div>
+        
+      </div>
+    </div>
   </div>
 );
 
